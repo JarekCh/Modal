fe1778a0df14ad66ec71f2d0293c63cdeec621e1
Jaroslaw Charchula <charchula@gmail.com>
2022-05-12 22:28:00 +0200

    add two listner event to test dynamic changinc values.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // app.js
 const showModal = document.querySelector('.show-modal');
-const showModalTwo = document.querySelector('.show-modal-two')
+const showModalTwo = document.querySelector('.show-modal-two');
+const showModalThree = document.querySelector('.show-modal-three');
 
-function modalBody(body) {
+function modalBody(body = {}) {
     return `<div class="modal">           
-    <p class="modal-title">
-      <i class="modal-title-icon" data-feather = "info"></i>
-      ${body.title}
+    <p class="modal-title ${body.type || ''}">
+      <i class="modal-title-icon" data-feather = "${body.modal_icon || 'info'}"></i>
+      ${body.title || 'are you sure'}
     </p>
     <p class="modal-description">
-        ${body.description}
+        ${body.description || 'confirm your choice'}
     </p>
     <div class="modal-buttons">
-        <button class="modal-accept">${body.accept_btn}</button>
-        <button class="modal-declinie">${body.decline_btn}</button>
+        <button class="modal-accept">${body.accept_btn || 'Yes'}</button>
+        <button class="modal-declinie">${body.decline_btn || 'No'}</button>
     </div>
     <button class="modal-close-icon">
         <i data-feather="x"></i>
@@ -33,6 +34,8 @@ function createModal(value) {
 
 function addToBasket(){
     createModal({
+        type: 'warning',
+        modal_icon: 'alert-triangle',
         title: 'lorem ipsum dolor title',
         description: 'lorem ipsum descriptopn, add more text, add more text, add more text, add more text,',
         accept_btn: 'Accept',
@@ -44,10 +47,14 @@ function addToBasket(){
 
 showModal.addEventListener('click', addToBasket);
 showModalTwo.addEventListener('click', () => {
-    createModal({
+    createModal({   
         title: 'modal two',
         description: 'second modal text, second modal text, second modal text, second modal text, ',
         accept_btn: 'Accept_two',
         decline_btn: 'Decline_tow',
     });
+});
+
+showModalThree.addEventListener('click', () => {
+    createModal();
 });
