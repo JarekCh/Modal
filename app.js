a48a38929b08cf870185b7ef3f54d9774d88b9c5
Jaroslaw Charchula <charchula@gmail.com>
2022-05-12 21:44:04 +0200

    add const variable, two event listeners, functions modalCreate, modalBody, addToBasket

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
 // app.js
+const showModal = document.querySelector('.show-modal');
+const showModalTwo = document.querySelector('.show-modal-two')
 
-feather.replace();
+function modalBody(body) {
+    return `<div class="modal">           
+    <p class="modal-title">
+      <i class="modal-title-icon" data-feather = "info"></i>
+      ${body.title}
+    </p>
+    <p class="modal-description">
+        ${body.description}
+    </p>
+    <div class="modal-buttons">
+        <button class="modal-accept">${body.accept_btn}</button>
+        <button class="modal-declinie">${body.decline_btn}</button>
+    </div>
+    <button class="modal-close-icon">
+        <i data-feather="x"></i>
+    </button>
+</div>`
+}
+
+function createModal(value) {
+    const modalContainer =  document.createElement('div');
+    modalContainer.className = 'modal-container';
+    modalContainer.innerHTML =  modalBody(value);
+    document.body.appendChild(modalContainer);
+
+    feather.replace();
+
+}
+
+function addToBasket(){
+    createModal({
+        title: 'lorem ipsum dolor title',
+        description: 'lorem ipsum descriptopn, add more text, add more text, add more text, add more text,',
+        accept_btn: 'Accept',
+        decline_btn: 'Decline',
+    });
+
+};
+
+
+showModal.addEventListener('click', addToBasket);
+showModalTwo.addEventListener('click', () => {
+    createModal({
+        title: 'modal two',
+        description: 'second modal text, second modal text, second modal text, second modal text, ',
+        accept_btn: 'Accept_two',
+        decline_btn: 'Decline_tow',
+    });
+});
